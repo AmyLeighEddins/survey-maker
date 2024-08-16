b4aff0b21f3934283a7ac0f83ed2c1126d8e1333
Amy Eddins <amyleigheddins@gmail.com>
2024-08-16 14:16:58 -0500

    Add basic routes to client

---
+++ client/src/routes/login/index.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../components/shared/Sidebar";
+
+const LogIn = () => {
+  return (
+    <>
+      <Sidebar />
+      <div>
+        <h1>Log In</h1>
+      </div>
+      </>
+  )
+}
+
+export default LogIn;
