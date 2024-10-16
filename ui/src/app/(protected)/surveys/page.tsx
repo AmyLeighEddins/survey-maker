7d470e8076ce67e8eb9c59b5624832efb469413f
Amy Eddins <amyleigheddins@gmail.com>
2024-10-16 17:26:11 -0500

    Add filtering to surveys and templates view all pages

--- ui/src/app/(protected)/surveys/page.tsx
+++ ui/src/app/(protected)/surveys/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { SyntheticEvent, useEffect, useState } from "react";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 import { ListFilter, PlusCircle, MoreHorizontal } from "lucide-react";
@@ -15,6 +16,11 @@ import useGetSurveys from "@/hooks/api/surveys/useGetSurveys";
 import useGetSurveyTypes from "@/hooks/api/types/useGetSurveyTypes";
 
 const Surveys = () => {
+  const [filteredSurveys, setFilteredSurveys] = useState<Survey[]>([]);
+  const [statusFilters, setStatusFilters] = useState<string>('all');
+  const [typeFilters, setTypeFilters] = useState<number[]>([]);
+  const [showAllTypes, setShowAllTypes] = useState<boolean>(true);
+
   const router = useRouter();
   const surveys = useGetSurveys();
   const types = useGetSurveyTypes();
@@ -22,6 +28,32 @@ const Surveys = () => {
   const isPending = surveys.isPending || types.isPending;
   const isFetching = surveys.isFetching || types.isFetching;
   const error = surveys.error?.message || types.error?.message;
+
+  useEffect(() => {
+    // Set the default type filters to all types on initial loading of data
+    setTypeFilters(types.data?.map((type: SurveyType) => type.id));
+  }, [types.data]);
+
+  useEffect(() => {
+    const now = new Date().toISOString();
+    let surveysFilteredByStatus = [];
+
+    // Filter by status
+    switch (statusFilters) {
+      case 'active':
+        surveysFilteredByStatus = surveys.data.filter((survey: Survey) => new Date(survey.expiry_date).toISOString() > now);
+        break;
+      case 'expired':
+        surveysFilteredByStatus = surveys.data.filter((survey: Survey) => new Date(survey.expiry_date).toISOString() < now);
+        break;
+      default:
+        surveysFilteredByStatus = surveys.data;
+        break;
+    };
+
+    // Then filter by type
+    setFilteredSurveys(surveysFilteredByStatus?.filter((survey: Survey) => typeFilters?.includes(survey.survey_type_id)));
+  }, [surveys.data, statusFilters, typeFilters]);
 
   if (isPending || isFetching) {
     return <div>Loading...</div>;
@@ -39,18 +71,31 @@ const Surveys = () => {
     router.push(`/surveys/${id}/edit`);
   };
 
+  const onClickTypeFilterOption = (checked: boolean, surveyTypeId: number) => {
+    const updateTypeFilters = checked ? [...typeFilters, surveyTypeId] : typeFilters.filter((type) => type !== surveyTypeId);
+    setTypeFilters(updateTypeFilters);
+    // If not all types are selected, uncheck the "Select All" checkbox
+    if (updateTypeFilters.length !== types.data?.length) setShowAllTypes(false);
+  };
+
+  const onClickShowAll = (checked: boolean) => {
+    setShowAllTypes(checked);
+    if (checked) {
+      setTypeFilters(types.data?.map((type: SurveyType) => type.id));
+    } else {
+      setTypeFilters([]);
+    }
+  };
+
   return (
     <div className="h-screen my-5">
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <Tabs defaultValue="all">
           <div className="flex items-center">
             <TabsList>
-              <TabsTrigger value="all">All</TabsTrigger>
-              <TabsTrigger value="active">Active</TabsTrigger>
-              <TabsTrigger value="draft">Draft</TabsTrigger>
-              <TabsTrigger value="archived" className="hidden sm:flex">
-                Archived
-              </TabsTrigger>
+              <TabsTrigger value="all" onClick={() => setStatusFilters('all')}>All</TabsTrigger>
+              <TabsTrigger value="active" onClick={() => setStatusFilters('active')}>Active</TabsTrigger>
+              <TabsTrigger value="expired" onClick={() => setStatusFilters('expired')}>Expired</TabsTrigger>
             </TabsList>
             <div className="ml-auto flex items-center gap-2">
               <DropdownMenu>
@@ -63,15 +108,15 @@ const Surveys = () => {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                  <DropdownMenuLabel>Filter by Type</DropdownMenuLabel>
                   <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem checked>
-                    Active
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>
-                    Archived
-                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem checked={showAllTypes} onCheckedChange={onClickShowAll}>Select All</DropdownMenuCheckboxItem>
+                  <DropdownMenuSeparator />
+                  {types.data?.map((type: SurveyType) => (
+                    <DropdownMenuCheckboxItem key={type.id} checked={typeFilters?.includes(type.id)} onCheckedChange={(value) => onClickTypeFilterOption(value, type.id)}>
+                      {type.description}
+                    </DropdownMenuCheckboxItem>
+                  ))}
                 </DropdownMenuContent>
               </DropdownMenu>
               <Button size="sm" className="h-7 gap-1">
@@ -82,7 +127,7 @@ const Surveys = () => {
               </Button>
             </div>
           </div>
-          <TabsContent value="all">
+          <div className="mt-2">
             <Card x-chunk="dashboard-06-chunk-0">
               <CardHeader>
                 <CardTitle>Surveys</CardTitle>
@@ -91,58 +136,62 @@ const Surveys = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Summary</TableHead>
-                      <TableHead>Created on</TableHead>
-                      <TableHead>Expires on</TableHead>
-                      <TableHead>Type</TableHead>
-                      <TableHead>
-                        <span className="sr-only">Actions</span>
-                      </TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    { surveys.data?.map((survey: Survey) => (
-                      <TableRow key={survey.id}>
-                        <TableCell className="font-medium">
-                          {survey.summary}
-                        </TableCell>
-                        <TableCell>{format(new Date(survey.created_date), 'MM/dd/yyyy')}</TableCell>
-                        <TableCell>{format(new Date(survey.expiry_date), 'MM/dd/yyyy')}</TableCell>
-                        <TableCell className="hidden md:table-cell">
-                          <Badge className="text-xs" variant="outline">
-                            {types.data?.find((type: SurveyType) => type.id === survey.survey_type_id)?.description}
-                          </Badge>
-                        </TableCell>
-                        <TableCell>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                aria-haspopup="true"
-                                size="icon"
-                                variant="ghost"
-                              >
-                                <MoreHorizontal className="h-4 w-4" />
-                                <span className="sr-only">Toggle menu</span>
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                              <DropdownMenuItem onClick={onViewClick(survey.id)}>View</DropdownMenuItem>
-                              <DropdownMenuItem onClick={onEditClick(survey.id)}>Edit</DropdownMenuItem>
-                              <DropdownMenuItem>Delete</DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </TableCell>
+                {filteredSurveys?.length ? (
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Summary</TableHead>
+                        <TableHead>Created on</TableHead>
+                        <TableHead>Expires on</TableHead>
+                        <TableHead>Type</TableHead>
+                        <TableHead>
+                          <span className="sr-only">Actions</span>
+                        </TableHead>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
+                    </TableHeader>
+                    <TableBody>
+                      {filteredSurveys.map((survey: Survey) => (
+                        <TableRow key={survey.id}>
+                          <TableCell className="font-medium">
+                            {survey.summary}
+                          </TableCell>
+                          <TableCell>{format(new Date(survey.created_date), 'MM/dd/yyyy')}</TableCell>
+                          <TableCell>{format(new Date(survey.expiry_date), 'MM/dd/yyyy')}</TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            <Badge className="text-xs" variant="outline">
+                              {types.data?.find((type: SurveyType) => type.id === survey.survey_type_id)?.description}
+                            </Badge>
+                          </TableCell>
+                          <TableCell>
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  aria-haspopup="true"
+                                  size="icon"
+                                  variant="ghost"
+                                >
+                                  <MoreHorizontal className="h-4 w-4" />
+                                  <span className="sr-only">Toggle menu</span>
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuItem onClick={onViewClick(survey.id)}>View</DropdownMenuItem>
+                                <DropdownMenuItem onClick={onEditClick(survey.id)}>Edit</DropdownMenuItem>
+                                <DropdownMenuItem>Delete</DropdownMenuItem>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                ) : (
+                  <div>No surveys found.</div>
+                )}
               </CardContent>
             </Card>
-          </TabsContent>
+          </div>
         </Tabs>
       </main>
     </div>
--- ui/src/app/(protected)/templates/page.tsx
+++ ui/src/app/(protected)/templates/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 import { ListFilter, PlusCircle, MoreHorizontal } from "lucide-react";
@@ -15,6 +16,10 @@ import useGetTemplates from "@/hooks/api/templates/useGetTemplates";
 import useGetSurveyTypes from "@/hooks/api/types/useGetSurveyTypes";
 
 const Templates = () => {
+  const [filteredTemplates, setFilteredTemplates] = useState<Template[]>([]);
+  const [typeFilters, setTypeFilters] = useState<number[]>([]);
+  const [showAllTypes, setShowAllTypes] = useState<boolean>(true);
+
   const router = useRouter();
   const templates = useGetTemplates();
   const types = useGetSurveyTypes();
@@ -22,6 +27,15 @@ const Templates = () => {
   const isPending = templates.isPending || types.isPending;
   const isFetching = templates.isFetching || types.isFetching;
   const error = templates.error?.message || types.error?.message;
+
+  useEffect(() => {
+    // Set the default type filters to all types on initial loading of data
+    setTypeFilters(types.data?.map((type: SurveyType) => type.id));
+  }, [types.data]);
+
+  useEffect(() => {
+    setFilteredTemplates(templates.data?.filter((survey: Template) => typeFilters?.includes(survey.survey_type_id)));
+  }, [templates.data, typeFilters]);
 
   if (isPending || isFetching) {
     return <div>Loading...</div>;
@@ -39,19 +53,27 @@ const Templates = () => {
     router.push(`/templates/${id}/edit`);
   };
 
+  const onClickTypeFilterOption = (checked: boolean, surveyTypeId: number) => {
+    const updateTypeFilters = checked ? [...typeFilters, surveyTypeId] : typeFilters.filter((type) => type !== surveyTypeId);
+    setTypeFilters(updateTypeFilters);
+    // If not all types are selected, uncheck the "Select All" checkbox
+    if (updateTypeFilters.length !== types.data?.length) setShowAllTypes(false);
+  };
+
+  const onClickShowAll = (checked: boolean) => {
+    setShowAllTypes(checked);
+    if (checked) {
+      setTypeFilters(types.data?.map((type: SurveyType) => type.id));
+    } else {
+      setTypeFilters([]);
+    }
+  };
+
   return (
     <div className="h-screen my-5">
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <Tabs defaultValue="all">
           <div className="flex items-center">
-            <TabsList>
-              <TabsTrigger value="all">All</TabsTrigger>
-              <TabsTrigger value="active">Active</TabsTrigger>
-              <TabsTrigger value="draft">Draft</TabsTrigger>
-              <TabsTrigger value="archived" className="hidden sm:flex">
-                Archived
-              </TabsTrigger>
-            </TabsList>
             <div className="ml-auto flex items-center gap-2">
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
@@ -63,15 +85,15 @@ const Templates = () => {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                  <DropdownMenuLabel>Filter by Type</DropdownMenuLabel>
                   <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem checked>
-                    Active
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>
-                    Archived
-                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem checked={showAllTypes} onCheckedChange={onClickShowAll}>Select All</DropdownMenuCheckboxItem>
+                  <DropdownMenuSeparator />
+                  {types.data?.map((type: SurveyType) => (
+                    <DropdownMenuCheckboxItem key={type.id} checked={typeFilters?.includes(type.id)} onCheckedChange={(value) => onClickTypeFilterOption(value, type.id)}>
+                      {type.description}
+                    </DropdownMenuCheckboxItem>
+                  ))}
                 </DropdownMenuContent>
               </DropdownMenu>
               <Button size="sm" className="h-7 gap-1">
@@ -82,7 +104,7 @@ const Templates = () => {
               </Button>
             </div>
           </div>
-          <TabsContent value="all">
+          <div className="mt-2">
             <Card x-chunk="dashboard-06-chunk-0">
               <CardHeader>
                 <CardTitle>Templates</CardTitle>
@@ -91,60 +113,64 @@ const Templates = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                      <TableHead>Summary</TableHead>
-                      <TableHead>Created on</TableHead>
-                      <TableHead>Updated on</TableHead>
-                      <TableHead>Type</TableHead>
-                      <TableHead>
-                        <span className="sr-only">Actions</span>
-                      </TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    { templates.data?.map((template: Template) => (
-                      <TableRow key={template.id}>
-                        <TableCell className="font-medium">
-                          {template.name}
-                        </TableCell>
-                        <TableCell>{template.summary}</TableCell>
-                        <TableCell>{format(new Date(template.created_date), 'MM/dd/yyyy')}</TableCell>
-                        <TableCell>{format(new Date(template.updated_date), 'MM/dd/yyyy')}</TableCell>
-                        <TableCell className="hidden md:table-cell">
-                          <Badge className="text-xs" variant="outline">
-                            {types.data?.find((type: SurveyType) => type.id === template.survey_type_id)?.description}
-                          </Badge>
-                        </TableCell>
-                        <TableCell>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                aria-haspopup="true"
-                                size="icon"
-                                variant="ghost"
-                              >
-                                <MoreHorizontal className="h-4 w-4" />
-                                <span className="sr-only">Toggle menu</span>
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                              <DropdownMenuItem onClick={onViewClick(template.id)}>View</DropdownMenuItem>
-                              <DropdownMenuItem onClick={onEditClick(template.id)}>Edit</DropdownMenuItem>
-                              <DropdownMenuItem>Delete</DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </TableCell>
+                {filteredTemplates?.length ? (
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Summary</TableHead>
+                        <TableHead>Created on</TableHead>
+                        <TableHead>Updated on</TableHead>
+                        <TableHead>Type</TableHead>
+                        <TableHead>
+                          <span className="sr-only">Actions</span>
+                        </TableHead>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
+                    </TableHeader>
+                    <TableBody>
+                      {filteredTemplates?.map((template: Template) => (
+                        <TableRow key={template.id}>
+                          <TableCell className="font-medium">
+                            {template.name}
+                          </TableCell>
+                          <TableCell>{template.summary}</TableCell>
+                          <TableCell>{format(new Date(template.created_date), 'MM/dd/yyyy')}</TableCell>
+                          <TableCell>{format(new Date(template.updated_date), 'MM/dd/yyyy')}</TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            <Badge className="text-xs" variant="outline">
+                              {types.data?.find((type: SurveyType) => type.id === template.survey_type_id)?.description}
+                            </Badge>
+                          </TableCell>
+                          <TableCell>
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  aria-haspopup="true"
+                                  size="icon"
+                                  variant="ghost"
+                                >
+                                  <MoreHorizontal className="h-4 w-4" />
+                                  <span className="sr-only">Toggle menu</span>
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuItem onClick={onViewClick(template.id)}>View</DropdownMenuItem>
+                                <DropdownMenuItem onClick={onEditClick(template.id)}>Edit</DropdownMenuItem>
+                                <DropdownMenuItem>Delete</DropdownMenuItem>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                ) : (
+                  <div>No templates found.</div>
+                )}
               </CardContent>
             </Card>
-          </TabsContent>
+          </div>
         </Tabs>
       </main>
     </div>
